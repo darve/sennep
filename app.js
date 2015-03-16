bde8778436ef618f0d3c62344f6cad2f9d0309c9
David Woollard <dave@darve.co.uk>
2015-03-16 12:32:13 +0000

    Twitter authentication successful - need to refactor the main app file but it works so far.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express             = require('express'),
     app                 = express(),
     path                = require('path'),
     args                = {},
-
+    session             = require('express-session'),
     passport            = require('passport'),
     twitterStrategy     = require('passport-twitter').Strategy;
 
@@ -15,35 +15,60 @@ var express             = require('express'),
  * Serve static files from the app directory.
  */
 app.use('/', express.static(path.resolve(__dirname, 'app')));
-app.use('/', function(req, res, next) {
-  res.status(404).json('File not found');
-});
 
 
 /**
- * Define an authentication Strategy
+ * Initialise persistent sessions
  */
+app.use(session({
+    secret: 'hellofriends',
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+/**
+ * Passport configuration
+ */
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
 passport.use(new twitterStrategy({
     consumerKey: 'K6kZzTHsatcXgPieyEcotxfY4',
     consumerSecret: 'hgAYZPLMSTdK9wCS4PsSWzh5NhsniJKg8ZlN1UE6rVBXrqWpim',
-    callbackURL: 'http://fuckometer.co.uk/auth/twitter/authed'
-}, function(token, tokenSecret, profile, done){
-    console.log(arguments);
-});
+    callbackURL: 'http://127.0.0.1:3000/auth/twitter/authed'
+  },
+  function(token, tokenSecret, profile, done) {
+      return done(null, profile);
+  }
+));
+
 
 /**
  * Twitter authentication routes
  */
-app.get('/auth/twitter/', passport.authenticate('twitter'));
-app.get('/auth/twitter/authed', passport.authenticate('twitter'), { successRedirect: '/', failureRedirect('/login' }));
+app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/twitter/authed', passport.authenticate('twitter',
+    {
+        successRedirect: '/tweets',
+        failureRedirect: '/error'
+    }
+));
 
 /**
  * Index route
  */
-app.get('/', function(req, res) {
-  res.sendfile('index.html', { root: 'app' });
-});
-
+// app.get('/', function(req, res) {
+//   res.sendfile('index.html', { root: 'app' });
+// });
 
 /**
  * Get the arguments that the server was started with
@@ -52,6 +77,9 @@ process.argv.forEach(function (val, index, array) {
     if ( val.indexOf('=') !== -1 ) args[val.split('=')[0]] = val.split('=')[1];
 });
 
+app.use('/', function(req, res, next) {
+  res.status(404).json('File not found');
+});
 
 /**
  * Start the express server
