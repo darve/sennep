d6cad1332ad3d331661247ca59f5d76dc34005f3
David Woollard <dave@darve.co.uk>
2015-03-16 11:43:23 +0000

    Added passport and passport-twitter as dependencies. Added some test routes to test authing with the app.

--- app.js
+++ app.js
@@ -2,25 +2,48 @@
 /**
  * Basic static file server powered by Express.
  */
-var express     = require('express'),
-    app         = express(),
-    path        = require('path'),
-    args        = {};
+var express             = require('express'),
+    app                 = express(),
+    path                = require('path'),
+    args                = {},
+
+    passport            = require('passport'),
+    twitterStrategy     = require('passport-twitter').Strategy;
+
 
 /**
  * Serve static files from the app directory.
  */
-app.use("/", express.static(path.resolve(__dirname, "app")));
-app.use("/", function(req, res, next) {
+app.use('/', express.static(path.resolve(__dirname, 'app')));
+app.use('/', function(req, res, next) {
   res.status(404).json('File not found');
 });
+
+
+/**
+ * Define an authentication Strategy
+ */
+passport.use(new twitterStrategy({
+    consumerKey: 'K6kZzTHsatcXgPieyEcotxfY4',
+    consumerSecret: 'hgAYZPLMSTdK9wCS4PsSWzh5NhsniJKg8ZlN1UE6rVBXrqWpim',
+    callbackURL: 'http://fuckometer.co.uk/auth/twitter/authed'
+}, function(token, tokenSecret, profile, done){
+    console.log(arguments);
+});
+
+/**
+ * Twitter authentication routes
+ */
+app.get('/auth/twitter/', passport.authenticate('twitter'));
+app.get('/auth/twitter/authed', passport.authenticate('twitter'), { successRedirect: '/', failureRedirect('/login' }));
 
 /**
  * Index route
  */
 app.get('/', function(req, res) {
-  res.sendfile('index.html', { root: "app" });
+  res.sendfile('index.html', { root: 'app' });
 });
+
 
 /**
  * Get the arguments that the server was started with
@@ -28,6 +51,7 @@ app.get('/', function(req, res) {
 process.argv.forEach(function (val, index, array) {
     if ( val.indexOf('=') !== -1 ) args[val.split('=')[0]] = val.split('=')[1];
 });
+
 
 /**
  * Start the express server
