7d8e806d7428b2a2b6ce29c14ffcbb79522c66fc
David Woollard <dave@darve.co.uk>
2015-03-17 11:33:44 +0000

    A bit of application scaffolding. Added a template for the tweets. Added a bit of javascript that will likely kickstart the application.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -8,7 +8,25 @@
 
     'use strict';
 
-    var util = {};
+    var util = {},
+        tweets = [];
+
+
+    function init() {
+
+        // Try and populate the tweets array. If the response is empty then
+        // we know the user is not logged in.
+        $.get('/api/tweets', function(response){
+            console.log(response);
+        }, function(err){
+            console.log('error', arguments);
+        });
+    }
+
+
+    function render() {
+
+    }
 
 
     // DOM has loaded, fire off the init function.
