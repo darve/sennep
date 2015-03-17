fbc054f12c84a106cd58b608df396484df4fd463
David Woollard <dave@darve.co.uk>
2015-03-17 16:08:29 +0000

    Cleaning everything up a bit. The responses from the server are now more uniform, and I've refactored a lot of the client side javascript.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,75 +1,79 @@
 
 /**
- * Smash targets below this line
+ * Sennep Coding Test.
  * -----------------------------
  */
 
 (function(win, doc, _) {
 
+
     'use strict';
+
 
     var util = {},
         tweets = [],
-
         $handle = $('#twitter-handle'),
         $tweets = $('#tweets'),
         $notLoggedIn = $('#not-logged-in'),
         $refresh = $('a.refresh'),
-
-        template = _.template($('#tweet-template').html()),
-        dummy = {
-            tweet_id: "123456",
-            avatar_url: '/assets/img/avatar.png',
-            display_name: 'Dave Hurricane',
-            twitter_handle: '@davehurricane',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, illo quidem consequuntur inventore aliquid ab, sunt repellendus fugit blanditiis facere facilis.'
-        };
+        template = _.template($('#tweet-template').html());
 
 
+    /**
+     * When the DOM has finished loading, this function is fired.
+     */
     function init() {
+        getTweets();
+        $refresh.on('click', getTweets);
+    }
 
-        // Try and populate the tweets array. If the response is empty then
-        // we know the user is not logged in.
+
+    /**
+     * This makes a request for the favourited tweet data, and responds
+     * accordingly if there are errors / user not logged in.
+     */
+    function getTweets() {
+
+        // Reset our structure
+        tweets = [];
+        $tweets.html('');
+
         $.getJSON('/api/tweets', function(response){
-            console.log(typeof response);
-            if ( typeof response === 'object' ) {
-                parseTweets(response);
+
+            if ( 'tweets' in response ) {
+                $handle.text(handle);
+                parseTweets(response.tweets);
+            } else if ( 'message' in response ) {
+                console.log(response.message);
             }
+
         }, function(err){
             console.log('error', arguments);
         });
     }
 
+
+    /**
+     * Iterates through the list of tweets returned from the server
+     * and compiles each one into a template, then appends it to the
+     * tweet list in the DOM.
+     */
     function parseTweets(list) {
 
         for ( var i in list ) {
             tweets.push(template(list[i]));
         }
 
-        // for ( var i = 0, l = 6; i < l; i++ ) {
-        //     tweets.push(template(dummy));
-        // }
-
-        render();
-        showTweets();
-    }
-
-    function render() {
-        $tweets.html('');
         for ( var i in tweets )  {
             $tweets.append(tweets[i]);
         }
-    }
 
-    function showTweets() {
         $tweets.show();
     }
 
-    function updateTwitterHandle(handle) {
-        $handle.text(handle);
-    }
 
     // DOM has loaded, fire off the init function.
     $(init);
+
 
 })(window,document, _);
